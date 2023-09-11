c79d2c7dec4835c42eb31c1ed333665cb78ac435
Min Xu <min.xu.public@gmail.com>
2023-09-11 21:49:32 +0000

    add select test, step 1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # USE a;
 # CREATE TABLE speedtest (id int, one_k_vector vecf32(1024));
 # CREATE TABLE speedtest (id int, sequence_id int, token_id int, layer_id int, one_k_vector vecf32(1024));
+
 import binascii
 import time
 
@@ -15,7 +16,7 @@ num_inserts = 1024 * 8
 num_vector_per_insert = 5
 
 
-def to_db_binary(value, dim=None):
+def to_db_hex(value, dim=None):
     if value is None:
         return value
 
@@ -27,8 +28,47 @@ def to_db_binary(value, dim=None):
     # return value.tobytes()
     return binascii.b2a_hex(value)
 
+def correctness_test():
+    engine = create_engine("mysql+pymysql://root:111@127.0.0.1:6001/a")
 
-def run():
+    # insert 2 new rows
+    def get_max_id():
+        with engine.connect() as con:
+            rs = con.execute(text('SELECT MAX(id) FROM speedtest'))
+            max_id = next(rs)[0]
+        return max_id
+
+    new_id = get_max_id() + 1
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    data = [np.random.rand(vec_len), np.random.rand(vec_len)]
+    sql_insert = text("insert into speedtest (id, one_k_vector) values(:id, decode(:data,'hex') );")
+    for i, arr in enumerate(data):
+        print("XXX", new_id + i)
+        session.execute(sql_insert, {"id": new_id + i, "data": to_db_hex(arr)})
+    session.commit()
+
+    new_max_id = get_max_id()
+    assert new_max_id == new_id + 1
+
+    # select using string output
+    with engine.connect() as con:
+        rs = con.execute(text(f'SELECT * FROM speedtest WHERE id >= {new_id}'))
+        for row in rs:
+            print("XXXXXX", row)
+        import ipdb; ipdb.set_trace()
+
+    # select using hex output
+    with engine.connect() as con:
+        rs = con.execute(text(f'SELECT id, encode(one_k_vector, "hex") FROM speedtest WHERE id >= {new_id}'))
+        for row in rs:
+            print("XXXXXX", row)
+        import ipdb; ipdb.set_trace()
+
+
+
+def insert():
     engine = create_engine("mysql+pymysql://root:111@127.0.0.1:6001/a")
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -37,12 +77,14 @@ def run():
     for i in range(num_inserts * num_vector_per_insert):
         arr = np.random.rand(vec_len)
         # print(arr)
-        session.execute(sql_insert, {"id": i, "data": to_db_binary(arr)})
+        session.execute(sql_insert, {"id": i, "data": to_db_hex(arr)})
     session.commit()
 
 
+correctness_test()
+
 start = time.time()
-run()
+insert()
 duration = time.time() - start
 print(f"Result: vector dim={vec_len} vectors "
       f"inserted={num_inserts * num_vector_per_insert} "
