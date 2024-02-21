b9b728c04fe9303affbf91b45b3db30e4e9036ad
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2024-02-21 12:46:45 -0800

    pg support

--- sift128/query_mo_pg.py
+++ sift128/query_mo_pg.py
@@ -118,8 +118,8 @@ if __name__ == "__main__":
 
             print(f"Query {i + 1} completed in {duration:.4f}s")
             recalls.append(calc_recall(options["K"], [expected_results[i].astype(np.float32)], actual_result))
-            if i == 500:
-                break
+            # if i == 5000:
+            #     break
 
         avg_latency = round(np.mean(latencies), 4)
         avg_recall = round(np.mean(recalls), 4)
