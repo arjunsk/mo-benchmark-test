78d8dcf94c06df8b261045278e6375c143a11261
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2024-03-18 16:59:25 -0700

    vector index regression test

--- vec_insert_perf.py
+++ vec_insert_perf.py
@@ -1,45 +1,49 @@
-"""
-create database a;
-use a;
-create table t3(a int, b vecf32(128));
-"""
 import binascii
 import time
-
 import numpy as np
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 
-table_name = "t3"
+table_name = "speedtest"
 vec_len = 128
 num_inserts = 1000_000
-num_vector_per_insert = 1
 
 
 def to_db_binary(value):
     if value is None:
         return value
 
-    value = np.asarray(value, dtype='<f4')
+    value = np.asarray(value, dtype='<f')
+
     if value.ndim != 1:
         raise ValueError('expected ndim to be 1')
 
-    return binascii.b2a_hex(value.tobytes()).decode('utf-8')
+    return binascii.b2a_hex(value)
 
 
 def run():
-    # Using mysqlclient (MySQLdb)
-    engine = create_engine("mysql+mysqldb://root:111@127.0.0.1:6001/a")
+    # Connect using mysqlclient (MySQLdb)
+    engine = create_engine("mysql+mysqldb://root:111@127.0.0.1:6001/")
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    sql_insert = text("INSERT INTO t3 (a, b) VALUES (:id, decode(:data,'hex'));")
-    for i in range(num_inserts * num_vector_per_insert):
+    # Creating the database and table
+    session.execute(text("DROP DATABASE IF EXISTS vecdb;"))
+    session.commit()
+
+    session.execute(text("CREATE DATABASE vecdb;"))
+    session.execute(text("USE vecdb;"))
+    session.execute(text("DROP TABLE IF EXISTS "+table_name+";"))
+    session.execute(text("CREATE TABLE "+table_name+"(id INT, one_k_vector VARCHAR(1024));"))
+    session.commit()
+
+    sql_insert = text("INSERT INTO "+table_name+" (id, one_k_vector) VALUES (:id, :data);")
+    for i in range(num_inserts):
         arr = np.random.rand(vec_len)
         arr = np.asarray(arr, dtype='<f')
         session.execute(sql_insert, {"id": i, "data": to_db_binary(arr)})
         if i % 1000 == 0:
-            print(f"Inserted {i} strings")
+            print(f"inserted {i}")
     session.commit()
 
 
@@ -49,8 +53,8 @@ def main():
     duration = time.time() - start
 
     print(f"Result: vector dim={vec_len} vectors "
-          f"inserted={num_inserts * num_vector_per_insert} "
-          f"insert/second={num_inserts * num_vector_per_insert / duration}")
+          f"inserted={num_inserts} "
+          f"insert/second={num_inserts / duration}")
 
 
 if __name__ == "__main__":
