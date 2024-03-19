aaeeb81196bcbb62c319c320c7f13c804e53c26a
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2024-03-18 17:05:44 -0700

    vector index regression test

--- vec_insert_perf.py
+++ vec_insert_perf.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import sessionmaker
 table_name = "speedtest"
 vec_len = 128
 num_inserts = 1000_000
+max_duration_seconds = 6 * 60
 
 
 def to_db_binary(value):
@@ -30,28 +31,36 @@ def run():
 
     session.execute(text("CREATE DATABASE vecdb;"))
     session.execute(text("USE vecdb;"))
-    session.execute(text("DROP TABLE IF EXISTS "+table_name+";"))
-    session.execute(text("CREATE TABLE "+table_name+"(id INT, one_k_vector VECF32(128));"))
+    session.execute(text("DROP TABLE IF EXISTS " + table_name + ";"))
+    session.execute(text("CREATE TABLE " + table_name + "(id INT, one_k_vector VECF32(128));"))
     session.commit()
 
-    sql_insert = text("INSERT INTO "+table_name+" (id, one_k_vector) VALUES (:id, decode(:data,'hex'));")
+    start_time = time.time()
+    sql_insert = text("INSERT INTO " + table_name + " (id, one_k_vector) VALUES (:id, decode(:data,'hex'));")
     for i in range(num_inserts):
+        current_time = time.time()
+        if current_time - start_time > max_duration_seconds:
+            raise RuntimeError("Execution time exceeded 3 minutes. Panic and abort!")
+
         arr = np.random.rand(vec_len)
         arr = np.asarray(arr, dtype='<f')
         session.execute(sql_insert, {"id": i, "data": to_db_binary(arr)})
+
         if i % 1000 == 0:
             print(f"inserted {i} rows")
     session.commit()
 
-
-def main():
-    start = time.time()
-    run()
-    duration = time.time() - start
-
+    duration = time.time() - start_time
     print(f"Result: vector dim={vec_len} vectors "
           f"inserted={num_inserts} "
           f"insert/second={num_inserts / duration}")
+
+
+def main():
+    try:
+        run()
+    except RuntimeError as e:
+        print(e)
 
 
 if __name__ == "__main__":
